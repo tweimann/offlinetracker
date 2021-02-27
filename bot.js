08ec5aa0cd3d36cc4731b5faf9aa30fcaaedc00c
tweimann <63401747+tweimann@users.noreply.github.com>
2021-02-27 08:48:03 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var auth = require('./auth.json');
+var settings = require('./settings.json');
 var Discord = require('discord.js');
 var bot = new Discord.Client();
 
@@ -8,7 +8,7 @@ bot.once('ready', () => {
 		activity: {
 			name: ' ',
 			type: 'STREAMING',
-			url: 'https://www.twitch.tv/montanablack88'
+			url: 'https://www.twitch.tv/xqcow'
 		}
 	});
 	console.log('Ready!');
@@ -19,7 +19,7 @@ bot.login(auth.token);
 bot.on('message', message => {
 	var sender = message.author;
 	var msg = message.content.toUpperCase();
-	var prefix = '>';
+	var prefix = settings.prefix;
     
 	if (msg === prefix + 'PING') {
 		message.reply('Pong!').then(reply => {reply.delete({timeout: 5000})}).catch();
@@ -28,6 +28,17 @@ bot.on('message', message => {
 	}
 });
 
+setInterval(function() {
+
+	bot.guilds.cache.find().then(member => {
+		bot.channels.cache.find(channel => channel.name === 'surveillance').send(member);
+	});
+
+}, settings.interval);
+
 bot.on('presenceUpdate', (oldPresence, newPresence) => {
+	bot.channels.cache.find(channel => channel.name === 'surveillance').send('test');
 	newPresence.guild.channels.find(channel => channel.name === 'chat').send('lol');
+	console.log('test' + oldPresence + newPresence);
+	return
 });
