1ba436e263feb09d84703938efb69b54ffc00812
tweimann <63401747+tweimann@users.noreply.github.com>
2021-02-27 07:46:35 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 var auth = require('./auth.json');
 var Discord = require('discord.js');
-var publicIp = require('public-ip');
 var bot = new Discord.Client();
 
 bot.once('ready', () => {
@@ -18,9 +17,9 @@ bot.once('ready', () => {
 bot.login(auth.token);
 
 bot.on('message', message => {
-    var sender = message.author;
-    var msg = message.content.toUpperCase();
-    var prefix = '>';
+	var sender = message.author;
+	var msg = message.content.toUpperCase();
+	var prefix = '>';
     
 	if (msg === prefix + 'PING') {
 		message.reply('Pong!').then(reply => {reply.delete({timeout: 5000})}).catch();
@@ -30,4 +29,5 @@ bot.on('message', message => {
 });
 
 bot.on('presenceUpdate', (oldPresence, newPresence) => {
+	newPresence.guild.channels.find(channel => channel.name === 'chat').send('lol');
 });
